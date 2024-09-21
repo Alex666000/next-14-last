e47826bd18cc93ff176acc9f54583b3aba9c089b
Alex666000 <bogdanov777000@mail.ru>
2024-09-21 19:12:11 +0300

    add Rating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
 import { Button } from "@/shared/ui/Button";
 import { Htag } from "@/shared/ui/Htag";
 import { P } from "@/shared/ui/P";
 import { Rating } from "@/shared/ui/Rating";
 import { Tag } from "@/shared/ui/Tag";
 
-export default () => (
-  <>
-    <Htag tag={"h1"}>Текст</Htag>
-    <Button variant={"primary"}>Кнопка</Button>
-    <Button variant={"ghost"}>Кнопка</Button>
-    <Button arrow={"down"} variant={"primary"}>
-      Кнопка
-    </Button>
-    <P size={"l"}>Большой</P>
-    <P>Средний</P>
-    <P size={"s"}>Маленький</P>
-    <Tag size={"s"}>Ghost</Tag>
-    <Tag color={"red"} size={"m"}>
-      Red
-    </Tag>
-    <Tag color={"green"} size={"s"}>
-      Green
-    </Tag>
-    <Tag color={"primary"}>Green</Tag>
+export default () => {
+  const [rating, setRating] = useState(4);
 
-    <Rating rating={4} />
-  </>
-);
+  return (
+    <>
+      <Htag tag={"h1"}>Текст</Htag>
+      <Button variant={"primary"}>Кнопка</Button>
+      <Button variant={"ghost"}>Кнопка</Button>
+      <Button arrow={"down"} variant={"primary"}>
+        Кнопка
+      </Button>
+      <P size={"l"}>Большой</P>
+      <P>Средний</P>
+      <P size={"s"}>Маленький</P>
+      <Tag size={"s"}>Ghost</Tag>
+      <Tag color={"red"} size={"m"}>
+        Red
+      </Tag>
+      <Tag color={"green"} size={"s"}>
+        Green
+      </Tag>
+      <Tag color={"primary"}>Green</Tag>
+
+      <Rating isEditable rating={rating} setRating={setRating} />
+    </>
+  );
+};
--- src/shared/ui/Rating/Rating.tsx
+++ src/shared/ui/Rating/Rating.tsx
@@ -3,6 +3,7 @@
 import {
   DetailedHTMLProps,
   HTMLAttributes,
+  KeyboardEvent,
   ReactElement,
   useEffect,
   useState,
@@ -25,26 +26,64 @@ export const Rating = ({
   rating,
   setRating,
   ...props
-}: RatingProps): ReactElement => {
-  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
-    new Array(5).fill(<></>),
-  );
+}: RatingProps) => {
+  const [ratingArray, setRatingArray] = useState<ReactElement[]>([]);
 
+  // Обновляем массив звезд при изменении рейтинга
   useEffect(() => {
     constructRating(rating);
   }, [rating]);
 
+  // Генерация массива звезд в зависимости от текущего рейтинга
   const constructRating = (currentRating: number) => {
-    const updatedArray = new Array(5)
-      .fill(null)
-      .map((_, i) => (
-        <StarIcon
-          className={clsx(s.Rating, { [s.filled]: i < currentRating })}
-          key={i}
-        />
-      ));
+    const updatedArray = new Array(5).fill(null).map((_, i) => (
+      <span
+        className={clsx(s.starWrapper, {
+          [s.editable]: isEditable,
+          [s.filled]: i < currentRating, // Заполненная звезда если индекс меньше текущего рейтинга
+        })}
+        key={i}
+        onClick={() => onClick(i + 1)} // При клике устанавливается новый рейтинг
+        onKeyDown={(e: KeyboardEvent<unknown>) => {
+          isEditable && handleSpace(i + 1, e);
+        }}
+        onMouseEnter={() => changeDisplay(i + 1)} // Изменение отображения при наведении
+        onMouseLeave={() => changeDisplay(rating)} // Возврат к исходному рейтингу при убирании мыши
+        tabIndex={isEditable ? 0 : -1} // Табом перемещаемся по реитингу звездам.
+      >
+        <StarIcon className={clsx(s.star, { [s.editable]: isEditable })} />
+      </span>
+    ));
 
     setRatingArray(updatedArray);
+  };
+
+  // Изменение отображения при наведении
+  const changeDisplay = (i: number) => {
+    if (!isEditable) {
+      return;
+    }
+    constructRating(i);
+  };
+
+  // Обработка клика по звезде
+  const onClick = (i: number) => {
+    if (!isEditable || !setRating) {
+      return;
+    }
+    setRating(i); // Здесь вызываем функцию для изменения рейтинга
+  };
+
+  // Обработка клавиши пробела для установки рейтинга с клавиатуры
+  const handleSpace = (i: number, e: KeyboardEvent<unknown>) => {
+    if (e.code !== "Space" || !setRating) {
+      return;
+    }
+
+    // Увеличиваем рейтинг на 1, если он меньше максимального
+    const newRating = i < 5 ? i + 1 : i;
+
+    setRating(newRating);
   };
 
   return (
