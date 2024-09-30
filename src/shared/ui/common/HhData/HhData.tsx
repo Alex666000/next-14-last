3b5f869356568f9dc40bc4b14e8891250afe09f3
Alex666000 <bogdanov777000@mail.ru>
2024-09-30 19:56:32 +0300

    add HhData

--- src/shared/ui/common/HhData/HhData.tsx
+++ src/shared/ui/common/HhData/HhData.tsx
@@ -3,6 +3,11 @@ import { Card } from "@/shared/ui/Card/Card";
 
 import s from "./HhData.module.scss";
 
+// Регулярка для форматирования цены
+export const priceRu = (price: number): string => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " ₽";
+};
+
 type HhDataProps = {
   className?: string;
   count?: number;
@@ -20,39 +25,41 @@ export const HhData = ({
         <div className={s.title}>Всего вакансий</div>
         <div className={s.countValue}>{count}</div>
       </Card>
-      <Card className={s.salaryBlock}>
-        <div>
-          <div className={s.title}>Начальный</div>
-          <div className={s.countValue}>{juniorSalary}</div>
-          <div className={s.rate}>
-            <RateIcon className={s.filledSvg} />
-            <RateIcon />
-            <RateIcon />
+      <div className={s.salaryBlockWrapper}>
+        <Card className={s.salaryBlock}>
+          <div>
+            <div className={s.title}>Начальный</div>
+            <div className={s.countValue}>{priceRu(juniorSalary)}</div>
+            <div className={s.rate}>
+              <RateIcon className={s.filledSvg} />
+              <RateIcon />
+              <RateIcon />
+            </div>
           </div>
-        </div>
-      </Card>
-      <Card className={s.salaryBlock}>
-        <div>
-          <div className={s.title}>Средний</div>
-          <div className={s.countValue}>{middleSalary}</div>
-          <div className={s.rate}>
-            <RateIcon className={s.filledSvg} />
-            <RateIcon />
-            <RateIcon />
+        </Card>
+        <Card className={s.salaryBlock}>
+          <div>
+            <div className={s.title}>Средний</div>
+            <div className={s.countValue}>{priceRu(middleSalary)}</div>
+            <div className={s.rate}>
+              <RateIcon className={s.filledSvg} />
+              <RateIcon />
+              <RateIcon />
+            </div>
           </div>
-        </div>
-      </Card>
-      <Card className={s.salaryBlock}>
-        <div>
-          <div className={s.title}>Профессионал</div>
-          <div className={s.countValue}>{seniorSalary}</div>
-          <div className={s.rate}>
-            <RateIcon className={s.filledSvg} />
-            <RateIcon />
-            <RateIcon />
+        </Card>
+        <Card className={s.salaryBlock}>
+          <div>
+            <div className={s.title}>Профессионал</div>
+            <div className={s.countValue}>{priceRu(seniorSalary)}</div>
+            <div className={s.rate}>
+              <RateIcon className={s.filledSvg} />
+              <RateIcon />
+              <RateIcon />
+            </div>
           </div>
-        </div>
-      </Card>
+        </Card>
+      </div>
     </div>
   );
 };
